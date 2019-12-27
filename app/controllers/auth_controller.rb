997d823f810c3f02c07922ea3c31c39aab259ace
FredrickBarrett <f.barrett12@gmail.com>
2019-12-26 22:17:21 -0500

    added functionality to create users and started JWT auth

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,26 @@
+class AuthController < ApplicationController
+    def login
+        @user = User.find_by(username: login_params[:username])
+                if @user && @user.authenticate(login_params[:password])
+                    render json: tokenForAccount(@user) #{user: @user, token: token}
+                else
+                    render json: {errors: @user.errors.full_messages}
+                end
+    end
+
+    def persist
+        if request.headers['Authorization']
+            # encoded_token = request.headers['Authorization'].split(' ')[1]
+            # token = JWT.decode(encoded_token, secret)
+            user_id = getIdFromToken
+            @user = User.find(user_id)
+            render json: tokenForAccount(@user)
+        end
+    end
+
+    private
+
+    def login_params
+        params.require(:auth).permit(:username, :password)
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.create(user_params)
+        if @user.valid? 
+            user = @user
+            token = JWT.encode({user_id: user.id}, secret, 'HS256')
+            render json: {user: user, token: token}
+        else
+            render json: {errors: user.errors.full_messages}
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :name, :password)
+    end
+
+    def secret
+        ENV["JWT_SECRET_KEY"]
+    end
+    
 end
