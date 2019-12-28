d9dafc9b72bd45dc1ba14e3812c52f1a260be852
FredrickBarrett <f.barrett12@gmail.com>
2019-12-28 05:05:15 -0500

    added routes for login and auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :tasks
   resources :routines
   resources :users
+
+  post "login", to: "auth#login"
+  get "auth", to: "auth#persist"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
